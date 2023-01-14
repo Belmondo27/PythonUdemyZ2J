00250a1ef3ac79c42fbf668d437b71751f2f73e5
Belmondo27 <zekan27@gmail.com>
2023-01-15 00:19:08 +0100

    Zmienne

---
+++ basics/01 data types/01 variables declaration.py
@@ -0,0 +1,25 @@
+number = 100
+
+print(number)
+print(number)
+
+
+print(number*3)
+
+number = 55
+print(number)
+
+number = "one"
+print(number)
+
+text = "Hello world"
+print(text + " Hello again")
+
+number = 70
+number2 = 10
+
+result = number + number2
+print(result)
+
+
+
---
+++ challanges/variables.py
@@ -0,0 +1,16 @@
+title = "Pierwszy email"
+content = "Hello world"
+sender = "Asia Kowalska"
+receiver = "Ola"
+emailDate = "2023.01.15"
+numAttachmens = 1
+attachmentName = "file.doc"
+
+print("Tytył maila: " + title)
+print("Treść emaila: ", content )
+print("Nadawca: ", sender)
+print("Odbiorca: ", receiver)
+print("Data maila: ", emailDate)
+print("Ilość załączników: ", numAttachmens)
+print("Załącznik numer 1: ", attachmentName)
+
