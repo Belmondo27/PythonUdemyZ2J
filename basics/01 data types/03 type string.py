0e16ea808cf95f56938f1379f5427009fdcbb887
Belmondo27 <zekan27@gmail.com>
2023-01-15 13:15:43 +0100

    typy danych cd

--- basics/01 data types/03 type string.py
+++ basics/01 data types/03 type string.py
@@ -10,3 +10,19 @@ print( str *4)
 
 strX3 = str * 3
 print(strX3)
+
+str2 = str + " and Hello again!"
+print(str2)
+
+print(str2[6:]) #od 6 indeksu do końca
+
+print(str2[::3]) #wyświetlanie co 3 znak
+
+multiLine = """Pierwsza linia 
+Druga linia
+Trzecia linia
+"""
+print(multiLine)
+
+multiLine2 = "Pierwsza linia\nDruga linia\nTrzecia \t linia   \" \\"  #znaki ucieki, tabluacji oraz cudzysów i backslash
+print(multiLine2)
---
+++ challanges/str test.py
@@ -0,0 +1,13 @@
+
+
+
+print("Wpisz imię: ")
+name = input()
+print("Wpisane imę: ", name)
+print("Wpisz nazwisko: ")
+surrname = input()
+print("Wpisane nazwisko: ", surrname)
+print("Wpisz miasto: ")
+city = input()
+print("Wpisane miasto: ", city)
+print("Nazywasz się", name, surrname, "i mieszkasz w mieście", city)
